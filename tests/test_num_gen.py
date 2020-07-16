86c14d20c5060a8578865420af97e6e54f25f5fe
r1220 <raffaele.1220@hotmail.com>
2020-07-16 19:06:11 +0100

    [edit] eidted test_rand.py and deleted test_num_rand.py as it was a duplicate during troubleshooting

--- tests/test_num_gen.py
+++
@@ -1,5 +0,0 @@
-from num_gen import generator
-
-def test_generator():
-    result = generator(1, 2)
-    assert result == 1 or result == 2
--- tests/test_rand.py
+++ tests/test_rand.py
@@ -1,5 +1,7 @@
 from num_gen import generator
 
-def test_gen_test():
-    result = generator(1, 2)
-    assert result == 1 or result == 2
+def test_generator():
+    for i in range(100):
+        result = generator(1, 2)
+        assert result == 1 or result == 2
+
