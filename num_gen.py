c890befdc2bf5b1d9d45fcac0b090e58dfec33bb
r1220 <raffaele.1220@hotmail.com>
2020-07-22 13:20:19 +0100

    [bug] function substitute modified in num_gen but still not working

--- num_gen.py
+++ num_gen.py
@@ -1,9 +1,11 @@
 import random
-from random import choice
 
 def generator(x,y):
     number = random.randint(x, y)
     return number
 def substitute(x,y,exclude):
-    numbers = choice( i for i in range(x, y) if i not in exclude)
-    return numbers
+    numbers = list(range(x, y))
+    numbers = numbers.remove(exclude)
+    number = random.choice(numbers)
+
+    return number
--- rules.py
+++ rules.py
@@ -4,9 +4,10 @@ from collections import Counter
 
 def rules():
     list = ball(5)
+
     c = Counter(list)
     for letter, count in c.most_common():
-        o = ('%s %7d' % (letter, count))
+        o = ('%s %7d' % (letter, count))  ##Needed to identify duplicates during debug
         if count > 2:
             list.remove(letter)
             list.extend(substitute(1, 4, letter))
