87853f253fdbe1b5ee13559b2c2b4007fc7e052d
r1220 <raffaele.1220@hotmail.com>
2020-07-22 11:59:57 +0100

    [edit] edit rules.py found a way to identify the numbers repeated, and output its number of repetitions

--- rules.py
+++ rules.py
@@ -1,14 +1,11 @@
 from draw_ball import ball
 from collections import Counter
+import collections
 
 def rules():
     list = ball(5)
-    count = Counter(list)
-    for i in list:
-        if list.count(i) > 2:  ## is not the array address but the item! WRONG!
-            list.pop(i)
-        new_list_len = len(list)
-        if new_list_len < 5:
-         list.append(ball(5 - new_list_len))
+    c = Counter(list)
+    for letter, count in c.most_common(2):
+        o = ('%s %7d' % (letter, count))
 
-    return list
+    return o
