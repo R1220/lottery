8d3008912936a7b45dc0472a2f4727a5702cf22e
r1220 <raffaele.1220@hotmail.com>
2020-07-22 12:45:00 +0100

    [edit] edited rules.py and num_gen.py able to find numbers repeated more than 2 times and substitued with another random number. exclusion same number not working yet

--- num_gen.py
+++ num_gen.py
@@ -3,3 +3,6 @@ import random
 def generator(x,y):
     number = random.randint(x, y)
     return number
+def substitute(x,y,exclude):
+    numbers = random.randint(x, y)
+    return numbers.remove(exclude)
--- rules.py
+++ rules.py
@@ -1,11 +1,14 @@
 from draw_ball import ball
+from num_gen import substitute
 from collections import Counter
-import collections
 
 def rules():
     list = ball(5)
     c = Counter(list)
-    for letter, count in c.most_common(2):
+    for letter, count in c.most_common():
         o = ('%s %7d' % (letter, count))
+        if count > 2:
+            list.remove(letter)
+            list.extend(substitute(1, 35, letter))
 
-    return o
+    return list
